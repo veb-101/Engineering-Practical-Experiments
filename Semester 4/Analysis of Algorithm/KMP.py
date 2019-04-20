134e78c82dc639c66b624365d477ba7cd91aa67d
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-04-21 01:13:40 +0530

    added KMP algorithm

--- Semester 4/Analysis of Algorithm/KMP.py
+++ Semester 4/Analysis of Algorithm/KMP.py
@@ -1,18 +1,27 @@
 # for proper examples refer https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
 # https://www.youtube.com/watch?v=V5-7GzOfADQ
 
+# uncomment print statements to see how the table is updated
+
 
 def prefixTable(pattern):
     prefix_table = [0]
 
     for i in range(1, len(pattern)):
         j = prefix_table[i-1]
+        # print(f"(i,j): {i, j}")
 
         while(j > 0 and pattern[j] != pattern[i]):
+            # print(f"In while j values: {j}, table[{j}]: {prefix_table[j-1]}, Pattern[{j}]: {pattern[j]}, Pattern[{i}]: {pattern[i]}")
             j = prefix_table[j-1]
+        # print(f"J: {j}, Pattern[{j}]: {pattern[j]}, Pattern[{i}]: {pattern[i]}")
         prefix_table.append(j+1 if pattern[j] == pattern[i] else j)
-
+        # print(f"Table: {prefix_table}")
+        # print()
     return prefix_table
+
+
+print(prefixTable("abadab"))
 
 
 def kmp(text, pattern):
