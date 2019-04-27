e9c6fb33d8ba31a2f735b4a06dd344f05af5b7d7
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-04-27 13:48:20 +0530

    Updated Analysis of Algorithm/mastersTheorem.py

--- Semester 4/Analysis of Algorithm/mastersTheorem.py
+++ Semester 4/Analysis of Algorithm/mastersTheorem.py
@@ -1,4 +1,3 @@
-# not complete
 from math import log
 
 print("Enter coefficient values of eqn a(T(n/b) * n^p * log(n)^k")
@@ -6,18 +5,17 @@ a = int(input("Enter a for the given recurrence eqn: "))
 b = int(input("Enter b for the given recurrence eqn: "))
 p = int(input("Enter p for the given recurrence eqn: "))
 k = int(input("Enter k for the given recurrence eqn: "))
-# fn = eval(fn)
 
-n_log_a_b = log(a, b)
-if n_log_a_b > p:
+log_a_b = log(a, b)
+if log_a_b > p:
     print("Case 1: slower growth")
-    print(f"E:{n_log_a_b - p}")
-    print(f"Solution:Theta(n ^{n_log_a_b})")
+    print(f"E:{log_a_b - p}")
+    print(f"Solution:Theta(n ^{log_a_b})")
 
-if n_log_a_b == p:
+if log_a_b == p:
     print("Case 2: Same growth")
-    print(f"Solution: Theta(n^{n_log_a_b}*log(n)^{k+1})")
+    print(f"Solution: Theta(n^{log_a_b}*log(n)^{k+1})")
 
-if n_log_a_b < p:
+if log_a_b < p:
     print("Case 3: Faster growth")
     print(f"Solution:Theta(n ^{p})")
