41154c6146684ec9ebecadb7641b92c0a6d8dbb9
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-06-22 20:29:46 +0530

    added new prefix function

--- Semester 4/Analysis of Algorithm/KMP.py
+++ Semester 4/Analysis of Algorithm/KMP.py
@@ -8,25 +8,32 @@ def prefixTable(pattern):
     prefix_table = [0]
 
     for i in range(1, len(pattern)):
-        j = prefix_table[i-1]
+        j = prefix_table[i - 1]
         # print(f"(i,j): {i, j}")
 
         while(j > 0 and pattern[j] != pattern[i]):
             # print(f"In while j values: {j}, table[{j}]: {prefix_table[j-1]}, Pattern[{j}]: {pattern[j]}, Pattern[{i}]: {pattern[i]}")
-            j = prefix_table[j-1]
+            j = prefix_table[j - 1]
         # print(f"J: {j}, Pattern[{j}]: {pattern[j]}, Pattern[{i}]: {pattern[i]}")
-        prefix_table.append(j+1 if pattern[j] == pattern[i] else j)
+        prefix_table.append(j + 1 if pattern[j] == pattern[i] else j)
         # print(f"Table: {prefix_table}")
         # print()
     return prefix_table
 
 
-print(prefixTable("abadab"))
+def prefixFunctionNaive(pattern):
+    current, result = -1, []
+    for x in pattern:
+        current += (-current, 1)[x == pattern[current]]
+        result.append(current)
+    return result
+
+
+print(prefixFunctionNaive("abadab"))
 
 
 def kmp(text, pattern):
-    table, ret, j = prefixTable(pattern), [], 0
-    print(table)
+    table, ret, j = prefixFunctionNaive(pattern), [], 0
     for i in range(len(text)):
         while j > 0 and text[i] != pattern[j]:
             j = table[j - 1]
@@ -38,6 +45,6 @@ def kmp(text, pattern):
     return ret
 
 
-print(kmp("badbabababadaa", "ababada"))
+print(kmp("badbabababadaa", "ab"))
 
 # Time complexity:O(m+n)
