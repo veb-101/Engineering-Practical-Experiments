af1c157e92e9d0b6d5b02ff64f0d88a8ffe8c4af
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-10-10 13:43:39 +0530

    added exp3-B: ;HEX to BCD

---
+++ Semester 5/Microprocessors/exp3-B.asm
@@ -0,0 +1,37 @@
+;HEX to BCD
+.model small
+.data
+    m1 db 10, 13, "Equivalent BCD no. is: $"
+    no dw 0ffffh
+
+.code
+    Disp macro xx
+        mov ah, 09
+        lea dx, xx
+        int 21h
+        endm
+    
+    .startup
+        Disp m1
+        mov cl, 0
+        mov dx, 0
+        mov ax, no
+        
+        mov bx, 0ah
+        back:
+            mov dx, 0
+            div bx
+            push dx
+            inc cl
+            cmp ax, 0
+            jnz back
+        
+        back1:
+            pop dx
+            add dl, 30h
+            mov ah, 02
+            int 21h
+            dec cl
+        jnz back1
+    .exit
+end
