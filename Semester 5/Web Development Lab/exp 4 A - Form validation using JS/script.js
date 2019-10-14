63d90be16252c0a207a3ea3e4f7d7902c58b460d
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-10-14 21:23:53 +0530

    added exp (4-a) basic JS form validation

---
+++ Semester 5/Web Development Lab/exp 4 A - Form validation using JS/script.js
@@ -0,0 +1,14 @@
+function validate() {
+    if (document.getElementById("Name").value == "") {
+        alert("Please provide your name!");
+    }
+    if (document.getElementById("E-mail").value == "") {
+        alert("Please provide your Email!");
+    }
+    if (document.getElementById("Phone").value == "") {
+        alert("Please provide your Phone no.!");
+    }
+    if (document.getElementById("City").value == "") {
+        alert("Please enter your city name.!");
+    }
+}
