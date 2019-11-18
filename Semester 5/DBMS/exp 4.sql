1e35b862093897c166c93655d2506be7c0567349
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-11-18 06:16:01 +0530

    updated ddl commands with more eg.

--- Semester 5/DBMS/exp 4.sql
+++ Semester 5/DBMS/exp 4.sql
@@ -5,20 +5,24 @@ alter table Person add age varchar(2);
 --truncate table Person;
 --drop table Person
 -- EXEC sp_help 'Person'
+alter table Person rename to Per;
+-- alter table Per rename column age to AGE;
+-- alter table Per drop column AGE;
+-- alter table Per modify age varchar(3);
 
 ----------DML Commands---------
-insert into Person values('Vaibhav', 'Goregaon', 10);
-insert into Person values('Lokesh', 'Kandivali', 10);
-insert into Person values('Hades', 'HELL', 10);
-insert into Person values('Zeus', 'Olympus', 10);
-select * from Person;
+insert into Per values('Vaibhav', 'Goregaon', 10);
+insert into Per values('Lokesh', 'Kandivali', 10);
+insert into Per values('Hades', 'HELL', 10);
+insert into Per values('Zeus', 'Olympus', 10);
+select * from Per;
 select ' -----------------------------';
-update Person set address='NAGALAND' where address='Kandivali';
-select * from Person;
+update Per set address='NAGALAND' where address='Kandivali';
+select * from Per;
 select ' -----------------------------';
-insert into Person(name, address) values ('Test', 'T20');
-update Person set age=10 where name='Test';
-select * from Person;
+insert into Per(name, address) values ('Test', 'T20');
+update Per set age=10 where name='Test';
+select * from Per;
 select ' -----------------------------';
-delete from Person where name='Vaibhav';
-select * from Person; 
+delete from Per where name='Vaibhav';
+select * from Per; 
