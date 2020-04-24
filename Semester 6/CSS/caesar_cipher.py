9035987ba25c2ef6195fdef68986acfaa40dd49b
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2020-04-24 18:39:12 +0530

    added Caesar Cipher sem6/CSS

---
+++ Semester 6/CSS/caesar_cipher.py
@@ -0,0 +1,32 @@
+import string
+
+char_num_map = {j: i for i, j in enumerate(string.ascii_lowercase + " ")}
+num_char_map = {i: j for i, j in enumerate(string.ascii_lowercase + " ")}
+
+
+def encrypt(text, key):
+    encrypted = ""
+    text = text.lower()
+    for letter in text:
+        letter_key = char_num_map[letter]
+        rotation = (letter_key + key) % 27
+        encrypted += num_char_map[rotation]
+    return encrypted
+
+
+def decrypt(text, key):
+    decrypted = ""
+    text = text.lower()
+    for letter in text:
+        letter_key = char_num_map[letter]
+        rotation = (letter_key - key) % 27
+        decrypted += num_char_map[rotation]
+    return decrypted
+
+
+if __name__ == "__main__":
+    message = input("Enter text: ")
+    encrypted_message = encrypt(message, 3)
+    decrypted_message = decrypt(encrypted_message, 3)
+    print(encrypted_message)
+    print(decrypted_message)
